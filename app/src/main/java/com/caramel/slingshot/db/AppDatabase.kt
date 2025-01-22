81e999f056551074ea94665fef5222b66a653103
achyuth13 <achyuthgollapudi@gmail.com>
2025-01-22 13:49:04 +0530

    Change room version and comment demo files

--- app/src/main/java/com/caramel/slingshot/db/AppDatabase.kt
+++ app/src/main/java/com/caramel/slingshot/db/AppDatabase.kt
@@ -1,19 +1,19 @@
-package com.caramel.slingshot.db
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import com.caramel.slingshot.model.Player
-
-@Database(entities = [Player::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun playerDao(): PlayerDao
-}
-
-//Initialisation
-//val db = Room.databaseBuilder(
-//    applicationContext,
-//    AppDatabase::class.java, "player-database"
-//).build()
+//package com.caramel.slingshot.db
 //
-//val playerDao = db.playerDao()
-
+//import androidx.room.Database
+//import androidx.room.RoomDatabase
+//import com.caramel.slingshot.model.Player
+//
+//@Database(entities = [Player::class], version = 1)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun playerDao(): PlayerDao
+//}
+//
+////Initialisation
+////val db = Room.databaseBuilder(
+////    applicationContext,
+////    AppDatabase::class.java, "player-database"
+////).build()
+////
+////val playerDao = db.playerDao()
+//
