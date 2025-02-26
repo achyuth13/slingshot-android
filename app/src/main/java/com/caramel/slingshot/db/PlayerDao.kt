770be2f3bc7b68ad0dda556471f303e7f7913bf6
Achyuth Nag <achyuthgollapudi@gmail.com>
2025-02-26 22:52:38 +0530

    Update PlayerDao.kt

--- app/src/main/java/com/caramel/slingshot/db/PlayerDao.kt
+++ app/src/main/java/com/caramel/slingshot/db/PlayerDao.kt
@@ -1,24 +1,32 @@
-//package com.caramel.slingshot.db
-//
-//import androidx.room.Dao
-//import androidx.room.Delete
-//import androidx.room.Insert
-//import androidx.room.Query
-//import androidx.room.Update
-//import com.caramel.slingshot.model.Player
-//
-//@Dao
-//interface PlayerDao {
-//
-//    @Insert
-//    suspend fun insertPlayer(player: Player)
-//
-//    @Query("SELECT * FROM players")
-//    suspend fun getAllPlayers(): List<Player>
-//
-//    @Delete
-//    suspend fun deletePlayer(player: Player)
-//
-//    @Update
-//    suspend fun updatePlayer(player: Player)
-//}
+// package com.caramel.slingshot.db;
+
+// import androidx.lifecycle.LiveData;
+// import androidx.room.Dao;
+// import androidx.room.Delete;
+// import androidx.room.Insert;
+// import androidx.room.Query;
+// import androidx.room.Update;
+
+// import com.caramel.slingshot.model.TaskEntity;
+
+// import java.util.List;
+
+// @Dao
+// public interface TaskDao {
+
+//     @Insert
+//     void insert(TaskEntity task);
+
+//     @Update
+//     void update(TaskEntity task);
+
+//     @Delete
+//     void delete(TaskEntity task);
+
+//     @Query("UPDATE `task-entity` SET isCompleted = :isCompleted WHERE id = :id")
+//     void updateTaskStatus(int id, boolean isCompleted);
+
+//     @Query("SELECT * FROM `task-entity`")
+//     LiveData<List<TaskEntity>> getAllTasks();
+
+// }
