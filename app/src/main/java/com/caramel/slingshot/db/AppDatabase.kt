14f171cba2ddfff3f43166734a732dc00d447d8d
Achyuth Nag <achyuthgollapudi@gmail.com>
2025-02-26 22:52:01 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/caramel/slingshot/db/AppDatabase.kt
+++ app/src/main/java/com/caramel/slingshot/db/AppDatabase.kt
@@ -1,19 +1,28 @@
-//package com.caramel.slingshot.db
-//
-//import androidx.room.Database
-//import androidx.room.RoomDatabase
-//import com.caramel.slingshot.model.Player
-//
-//@Database(entities = [Player::class], version = 1)
-//abstract class AppDatabase : RoomDatabase() {
-//    abstract fun playerDao(): PlayerDao
-//}
-//
-////Initialisation
-////val db = Room.databaseBuilder(
-////    applicationContext,
-////    AppDatabase::class.java, "player-database"
-////).build()
-////
-////val playerDao = db.playerDao()
-//
+// package com.caramel.slingshot.db;
+
+// import android.content.Context;
+
+// import androidx.room.Database;
+// import androidx.room.Room;
+// import androidx.room.RoomDatabase;
+
+// import com.caramel.slingshot.model.TaskEntity;
+
+// @Database(entities = {TaskEntity.class}, version = 1, exportSchema = false)
+// public abstract class TaskDatabase extends RoomDatabase {
+
+//     private static volatile TaskDatabase INSTANCE;
+
+//     public abstract TaskDao taskDao();
+
+//     public static TaskDatabase getInstance(final Context context) {
+//         if(INSTANCE == null) {
+//             synchronized (TaskDatabase.class) {
+//                 INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TaskDatabase.class, "task-db").build();
+//             }
+//         }
+//         return INSTANCE;
+//     }
+
+
+// }
